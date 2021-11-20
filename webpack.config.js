00feaee9bb6ffc7105efc45c2b3f42b1d1e27713
Dmitry Kiriushin <93088720+DmitryPower@users.noreply.github.com>
2021-11-20 18:40:12 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  watch: true,
+
+  devtool: "source-map",
+
+  module: {}
+};
